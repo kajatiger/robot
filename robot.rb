15fc4b4cff26ff436b5daa4249c8dff16da35e3e
kaja <k.santro@posteo.de>
2017-04-20 13:27:14 +0200

    changed the conditions

--- robot.rb
+++ robot.rb
@@ -29,35 +29,35 @@ class Robot
 	end
 
 	def move(position)
-		if @orientation = S
-			@position y = 0 ? if true return 
-			@position y = y - 1 
-		elsif @orientation = N
-			@position y = 5 ? if true return
-			@position y = y + 1
-		elsif @orientation = E 
-			@position x = 5 ? if true return
-			@position x = x + 5
+		if @orientation == S
+			@position y == 0 ? if true puts "I don't want to fal off the edge." return 
+			@position y == y - 1 
+		elsif @orientation == N
+			@position y == 5 ? if true puts "I don't want to fal off the edge." return
+			@position y == y + 1
+		elsif @orientation == E 
+			@position x == 5 ? if true puts "I don't want to fal off the edge." return
+			@position x == x + 5
 		else
-			@position x = 0 ? if true return
-			@position x = x - 1
+			@position x == 0 ? if true puts "I don't want to fal off the edge." return
+			@position x == x - 1
 		end						
 	end
 
 	def turn(direction)
-		if @orientation = S and direction = left
+		if @orientation == S and direction == "left"
 			@orientation = E 
-		elsif @orientation = S and direction = right
+		elsif @orientation == S and direction == "right"
 			@orientation = W
-		elsif @orientation = N and direction = left
+		elsif @orientation == N and direction == "left"
 			@orientation = W
-		elsif @orientation = N and direction = right
+		elsif @orientation == N and direction == "right"
 			@orientation = E
-		elsif @orientation E and direction = left
+		elsif @orientation == E and direction == "left"
 			@orientation = N
-		elsif @orientation = E and direction = right
+		elsif @orientation == E and direction == "right"
 			@orientation = S 
-		elsif @orientation = W and direction = left
+		elsif @orientation == W and direction == "left"
 			@orientation = S
 		else
 			@orientation = N
